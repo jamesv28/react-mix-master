2e6159d801753db631323497f113d4c55d7c7170
James Volmert <james.volmert@gmail.com>
2024-08-21 12:14:22 -0600

    form input for newsletter

--- src/pages/NewsletterView.jsx
+++ src/pages/NewsletterView.jsx
@@ -2,9 +2,48 @@ import React from "react";
 
 const NewsletterView = () => {
   return (
-    <div>
-      <h2>Newsletter</h2>
-    </div>
+    <form className="form">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+        Our Newsletter
+      </h4>
+      <div className="form-row">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          defaultValue="james"
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          defaultValue=""
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          className="form-input"
+          name="email"
+          id="email"
+          defaultValue=""
+        />
+      </div>
+      <button
+        className="btn btn-block"
+        type="submit"
+        style={{ marginTop: "0.5rem", padding: "0.75rem" }}
+      >
+        Submit
+      </button>
+    </form>
   );
 };
 
